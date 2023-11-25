1a1281e60cc45c512164e12aac6f0f75df607292
AbuBokorprog <abubokor1066@gmail.com>
2023-11-25 11:24:41 +0600

    task initialized

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      background: "#EBF0F4",
+      red: "#DF3737",
+      button: "#3A4B86",
+      white: "#FFFFFF",
+      blue: "#3FAEFD",
+      green: "#13ce66",
+      yellow: "#ffc82c",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+  },
+  plugins: [],
+};
+// require("daisyui")
